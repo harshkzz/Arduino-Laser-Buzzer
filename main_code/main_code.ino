95f3dca057381f806c7e88ff898f96baeab15060
Harsh Kumar <125388784+harshkzz@users.noreply.github.com>
2023-02-19 00:49:07 +0530

    Add files via upload

---
+++ main_code/main_code.ino
@@ -0,0 +1,22 @@
+#define pin_laser A1        // Change pin number here
+#define pin_buzzer 3 
+#define light_value 80     // The amount of light can depend upon
+                           // your environment and the laser module
+                           // used so change the value accordingly
+void setup(){
+  Serial.begin(9600);
+  pinMode(pin_laser, INPUT);    
+  pinMode(pin_buzzer, OUTPUT);
+}
+
+void loop(){
+  Serial.println(analogRead(pin_laser));
+  if(analogRead(pin_laser) > light_value){
+    tone(pin_buzzer, 500);
+    delay(100);
+  }
+  else{
+    noTone(pin_buzzer);
+  }
+}
+
---
+++ value_finder/value_finder.ino
@@ -0,0 +1,8 @@
+#define pin_laser A1                        // Change A1 here if you want to use any other analog pin
+void setup(){
+  Serial.begin(9600);
+  pinMode(pin_laser, INPUT);               // Setting the pin as input pin
+}
+void loop(){
+  Serial.println(analogRead(pin_laser));   // Reading the values from the pin and printing it on the serial monitor 
+}
